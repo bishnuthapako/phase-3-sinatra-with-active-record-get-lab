cbddc679c0cea230a22efd437f2f5bbc76cc871f
Bishnu <bishnuthapako@gmail.com>
2022-08-10 09:25:24 -0400

    commit tree f749236c8840817a15b40e955e8ab653bc5420e3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do
+    # "Hello World"
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json
+  end
 
 end
